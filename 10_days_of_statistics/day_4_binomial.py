26af3fbbcacbf3f9420d72392b151da42dbb030b
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-07-13 12:07:54 +0200

    feat(day4): add computation

---
+++ 10_days_of_statistics/day_4_binomial.py
@@ -0,0 +1,44 @@
+from math import factorial
+from re import split
+
+format_prec_three = "{:.3f}"
+
+
+def challenge_4():
+    input = split(" ", input())
+    prob_boys, prob_girls = float(input[0]), float(input[1])
+    p = prob_boys / (prob_boys + prob_girls)
+
+    sum = 0
+
+    for k in range(3, 7):
+        binom = factorial(6) / (factorial(k) * factorial(6 - k))
+        sum += binom * p ** k * (1 - p) ** (6 - k)
+
+    return format_prec_three.format(sum)
+
+
+def challenge_5():
+    input = split(" ", input())
+    error_prob, n_pistons = int(input[0])/100, int(input[1])
+
+    sum = 0
+
+    for k in range(1,3):
+        binom = factorial(n_pistons) / (factorial(k) * factorial(n_pistons - k))
+        sum += binom * error_prob ** k * (1 - error_prob) ** (n_pistons - k)
+
+    return format_prec_three.format(sum)
+
+
+def challenge_6():
+    input = input().split()
+    error_prob, n_pistons = int(input[0])/100, int(input[1])
+
+    sum = 0
+
+    for k in range(1,3):
+        binom = factorial(n_pistons) / (factorial(k) * factorial(n_pistons - k))
+        sum += binom * error_prob ** k * (1 - error_prob) ** (n_pistons - k)
+
+    return format_prec_three.format(sum)
---
+++ 10_days_of_statistics/day_4_geometric.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+format_prec_three = "{:.3f}"
+
+
+def challenge_geometric_distribution_2():
+    prob_numerator, prob_denominator = input().split()
+    latest_success = int(input())
+    p = float(prob_numerator) / float(prob_denominator)
+
+    result = 0
+
+    for k in range(latest_success):
+        result += p * (1 - p) ** k
+
+    print(format_prec_three.format(result))
